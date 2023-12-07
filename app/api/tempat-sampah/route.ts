3b81d83c9eeabec9b504c4d3ac68743f7ee0ac45
safiqq <szarifin20041@gmail.com>
2023-12-07 13:43:12 +0700

    feat: create api with prisma

---
+++ app/api/tempat-sampah/route.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export async function GET() {
+  try {
+    const data = await prisma.tempat_sampah.findMany();
+    return Response.json({ data });
+  } catch (error) {
+    return Response.json({ error });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+export async function POST(req: Request) {
+  try {
+    const payload = await req.json();
+    await prisma.tempat_sampah.create({data: payload});
+    return Response.json({message: "success"})
+  } catch (error) {
+    return Response.json({error})
+  }
+}
